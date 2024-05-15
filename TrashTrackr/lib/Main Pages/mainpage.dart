3ed2c998da2ec21dae8c71f4cdcdbee1146d0d5d
Francis <furandoryun@gmail.com>
2024-05-16 02:33:55 +0800

    main page new

--- TrashTrackr/lib/Main Pages/mainpage.dart
+++ TrashTrackr/lib/Main Pages/mainpage.dart
@@ -8,12 +8,31 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final MapController _mapController = MapController();
+  bool _isZoomingIn = false;
+  bool _isZoomingOut = false;
+
+  void _zoomIn() {
+    if (_isZoomingIn) {
+      _mapController.move(_mapController.center, _mapController.zoom + 0.1);
+      Future.delayed(Duration(milliseconds: 100), _zoomIn);
+    }
+  }
+
+  void _zoomOut() {
+    if (_isZoomingOut) {
+      _mapController.move(_mapController.center, _mapController.zoom - 0.1);
+      Future.delayed(Duration(milliseconds: 100), _zoomOut);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           FlutterMap(
+            mapController: _mapController,
             options: MapOptions(
               center: LatLng(13.6218, 123.19480),
               zoom: 14,
@@ -24,6 +43,42 @@ class _MainPageState extends State<MainPage> {
                 userAgentPackageName: 'com.example.app',
               ),
             ],
+          ),
+          Positioned(
+            top: 10.0,
+            right: 10.0,
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Icon(Icons.zoom_in),
+                  onLongPress: () {
+                    setState(() {
+                      _isZoomingIn = true;
+                    });
+                    _zoomIn();
+                  },
+                  onLongPressEnd: (details) {
+                    setState(() {
+                      _isZoomingIn = false;
+                    });
+                  },
+                ),
+                GestureDetector(
+                  child: Icon(Icons.zoom_out),
+                  onLongPress: () {
+                    setState(() {
+                      _isZoomingOut = true;
+                    });
+                    _zoomOut();
+                  },
+                  onLongPressEnd: (details) {
+                    setState(() {
+                      _isZoomingOut = false;
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       ),
